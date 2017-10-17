ee1416c60cc8c1c55b84c699ca97e025bc9a1524
jonthtang <jonthtang@gmail.com>
2017-10-17 17:44:54 +0800

    Added Controller and Views for show, edit, new and index

---
+++ app/controllers/tasklists_controller.rb
@@ -0,0 +1,48 @@
+class TasklistsController < ApplicationController
+
+  def index
+  @tasklists = Tasklist.all
+ end
+
+def show
+  @tasklist= Tasklist.find(params[:id])
+ end
+
+def new
+  @tasklist=Tasklist.new
+end
+
+def edit
+  @tasklist=Tasklist.find(params[:id])
+ end
+
+def create
+  @tasklist = Tasklist.new(tasklist_params)
+  if @tasklist.save
+    redirect_to tasklists_path
+  else
+    render :new
+  end
+ end
+
+def update
+  @tasklist=Tasklist.find(params[:id])
+  if @tasklist.update(tasklist_params)
+  redirect_to tasklists_path
+  else
+  render :edit
+  end
+ end
+
+def destroy
+  @tasklist=Tasklist.find(params[:id])
+  @tasklist.destroy
+  redirect_to tasklists_path
+ end
+
+private
+
+def tasklist_params
+  params.require(:tasklist).permit(:title)
+end
+end
