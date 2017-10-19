ed6dab97a5b9d3f8015e9358242e24d3ddeb7dc9
shirlaine <shirlaine.phang@gmail.com>
2017-10-19 16:07:29 +0800

    Create complete and incomplete for taskcontroller, and member routes for tasks, edit styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasklists, except: :show do
-    resources :tasks
+    resources :tasks do
+      put 'complete', on: :member
+      put 'incomplete', on: :member
+    end
   end
 
 end
